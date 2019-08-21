a307850d4f08080c5e0e615a076f515d2568cf79
Bingyan Sahoo <bingyan.sahoo@intimetec.com>
2019-08-21 16:36:22 +0530

    chanegs 1

--- SayHello.php
+++ SayHello.php
@@ -4,8 +4,9 @@ class SayHello
 {
     public static function world()
     {
-		$abc = 10;
-		$abcd = 100;
+		$abc = 150;
+		$abcd = 1000;
+		$cd = 1000;
 		$abcde = 1000;
         return 'Hello World, Composer!';
     }
